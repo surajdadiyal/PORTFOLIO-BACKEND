62411490d6ad87c23a355f6880e30a7ec737f64b
surajdadiyal <surajprince0009@gmail.com>
2025-02-01 17:56:15 +0530

    values commit

--- controllers/userController.js
+++ controllers/userController.js
@@ -91,6 +91,8 @@ export const logout = catchAsyncErrors(async (req, res, next) => {
         .cookie("token", "", {
             expires: new Date(Date.now()),
             httpOnly: true,
+            sameSite : "None",
+            secure : true
         }).json({
             success: true,
             message: "Logged Out",
